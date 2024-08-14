36967e7c60ed4ffacb5fdcfa5182a2c841a0c64d
luneria9 <luneria444@gmail.com>
2024-08-14 20:02:45 +0000

    modified the csv and added missing requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
+    page_title='Student Performance Analysis',
     page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
 )
 
